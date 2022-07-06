17e9734f40ea01b514fa5fa9b5c3bc3152f441ed
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2022-07-06 17:59:01 -0300

    Efeito Parallax Scroll hotizontal

---
+++ Assets/Aula 02 - Background Scroll/Scripts/Paisagem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paisagem : MonoBehaviour
+{
+    public float Velocidade = 0.1f;
+    public Renderer quad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(Velocidade * Time.deltaTime, 0);
+        quad.material.mainTextureOffset += offset;
+    }
+}
